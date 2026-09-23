e5195661b5b11d667455812449deda64162ac148
Wolf <Wolf@bush.works>
2026-09-23 01:36:43 -0600

    Fix initial database migration

--- migrations/versions/0824aa3bd9f6_initial_database_schema.py
+++ migrations/versions/0824aa3bd9f6_initial_database_schema.py
@@ -17,8 +17,74 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        "io_points",
+
+        sa.Column(
+            "id",
+            sa.Integer(),
+            nullable=False,
+        ),
+
+        sa.Column(
+            "tag_name",
+            sa.String(length=80),
+            nullable=False,
+        ),
+
+        sa.Column(
+            "io_type",
+            sa.String(length=2),
+            nullable=False,
+        ),
+
+        sa.Column(
+            "address",
+            sa.String(length=100),
+            nullable=False,
+        ),
+
+        sa.Column(
+            "description",
+            sa.String(length=255),
+            nullable=True,
+        ),
+
+        sa.Column(
+            "signal_min",
+            sa.Float(),
+            nullable=True,
+        ),
+
+        sa.Column(
+            "signal_max",
+            sa.Float(),
+            nullable=True,
+        ),
+
+        sa.Column(
+            "engineering_min",
+            sa.Float(),
+            nullable=True,
+        ),
+
+        sa.Column(
+            "engineering_max",
+            sa.Float(),
+            nullable=True,
+        ),
+
+        sa.Column(
+            "engineering_unit",
+            sa.String(length=20),
+            nullable=True,
+        ),
+
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("address"),
+        sa.UniqueConstraint("tag_name"),
+    )
 
 
 def downgrade():
-    pass
+    op.drop_table("io_points")
